2ba1cd03fb84292d7fe0c92c2a4962979d45acab
unknown <rcandrew@millersville.edu>
2020-01-11 18:10:56 -0500

    Movement Done

---
+++ UntitledSquidGame/Assets/ButtonDetect.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDetect : MonoBehaviour
+{
+    public static int buttonDoor1;
+    public static int buttonDoor2;
+    public static int keyGet;
+    public GameObject door1;
+    public GameObject door2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonDoor1 = 0;
+        buttonDoor2 = 0;
+        keyGet = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward, Color.green);
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider.gameObject.name == "Button1" && Input.GetButtonDown("Jump"))
+            {
+                buttonDoor1 = 1;
+            }
+            else if (hit.collider.gameObject.name == "Button2" && Input.GetButtonDown("Jump"))
+            {
+                buttonDoor2 = 1;
+            }
+        }
+        if (buttonDoor1 == 1)
+        {
+            Vector3 posChange1 = door1.transform.position;
+            posChange1.y -= 0.1f;
+            door1.transform.position = posChange1;
+        }
+        if (buttonDoor2 == 1 && keyGet == 1)
+        {
+            Vector3 posChange2 = door2.transform.position;
+            posChange2.y -= 0.1f;
+            door2.transform.position = posChange2;
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit other)
+    {
+        if (other.collider.gameObject.name == "key")
+        {
+            keyGet = 1;
+            Destroy(other.gameObject);
+        }
+
+
+    }
+}
---
+++ UntitledSquidGame/Assets/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    CharacterController characterController;
+    public float movementSpeed;
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 rotation;
+    public float smooth = 1f;
+    private Quaternion targetRotation;
+    public float rotateHelp;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        targetRotation = transform.rotation;
+    }
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(0, 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection * movementSpeed);
+            if (Input.GetKey("a"))
+            {
+                targetRotation *= Quaternion.AngleAxis(-rotateHelp, Vector3.up);
+            }
+            else if (Input.GetKey("d"))
+            {
+                targetRotation *= Quaternion.AngleAxis(rotateHelp, Vector3.up);
+            }
+            this.transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10 * smooth * Time.deltaTime);
+            
+        }
+        moveDirection.y -= 1f * Time.deltaTime;
+        characterController.Move(moveDirection);
+    }
+
+}
